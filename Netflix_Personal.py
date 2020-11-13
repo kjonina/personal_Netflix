7b563ca4bc628425c3d48a57d54dbed948450e8d
kjonina <kjonina@gmail.com>
2020-11-13 22:17:54 +0000

    Creating a new variable called Types, removing all trailers and hooks from the data

--- Netflix_Personal.py
+++ Netflix_Personal.py
@@ -24,39 +24,35 @@ from sklearn.feature_extraction.text import CountVectorizer #Bag of Words
 
 
 
-# The dataset used was collected from the following website:
+# The df used was collected from the following website:
 # https://www.kaggle.com/shivamb/netflix-shows
 
 # read the CSV file
-dataset = pd.read_csv('ViewingActivity.csv')
+df = pd.read_csv('ViewingActivity.csv')
 
 # Will ensure that all columns are displayed
 pd.set_option('display.max_columns', None) 
 
 # prints out the top 5 values for the datasef
-print(dataset.head())
+print(df.head())
 
-# checking the dataset shape
-print(dataset.shape)
+# checking the df shape
+print(df.shape)
 # (6234, 12)
 
 
 # prints out names of columns
-print(dataset.columns)
+print(df.columns)
 
 # This tells us which variables are object, int64 and float 64. This would mean that 
 # some of the object variables might have to be changed into a categorical variables and int64 to float64 
 # depending on our analysis.
-print(dataset.info())
+print(df.info())
 
 
 # checking for missing data
-dataset.isnull().sum() 
-
-# dropping null value columns to avoid errors 
-# You cant create a code to autofill the string data
-dataset.dropna(inplace = True) 
-#Profile Name                  0
+df.isnull().sum() 
+#Profileame N                  0
 #Start Time                    0
 #Duration                      0
 #Attributes                 6819
@@ -67,47 +63,60 @@ dataset.dropna(inplace = True)
 #Latest Bookmark               0
 #Country                       0
 
-# checking the dataset shape
-print(dataset.shape)
+
+
+
+# checking the df shape
+print(df.shape)
 # (10106, 10)
 
 
 
 # =============================================================================
-# Cleaning data
+# Droping Columns
 # =============================================================================
-dataset = dataset.drop(['Attributes', 'Bookmark', 'Latest Bookmark'], axis = 1)
+df = df.drop(['Attributes', 'Bookmark', 'Latest Bookmark', 'Country', 'Device Type'], axis = 1)
 
-print(dataset.head())
+print(df.head())
+
+df['SVT'] = df['Supplemental Video Type'].isnull()
+
+# Trying to create a dataset with only True as SVT
+df1 = df[df.SVT == True]
+df1 = df1.drop(['SVT', 'Supplemental Video Type'], axis = 1)
 
 
 # =============================================================================
 # DataTime
 # =============================================================================
+#checking data types
+df1.dtypes
 
-dataset.dtypes
+#changing Profile Name to a categorical variable
+df1['Profile Name'] = df1['Profile Name'].astype('category')
 
-dataset['Profile Name'] = dataset['Profile Name'].astype('category')
-
-dataset['Start Time'] = pd.to_datetime(dataset['Start Time'], utc = True)
-
-
-dataset.head()
+# changing Start Time to Date Time in UTC Time Zone
+df1['Start Time'] = pd.to_datetime(df1['Start Time'], utc = True)
 
 
-dataset['Duration'] = pd.to_timedelta(dataset['Duration'])
+df1.head()
 
-dataset['Duration'].groupby(dataset['Profile Name']).sum()
-#Karina     90 days 06:56:55
-#Karolina   21 days 22:49:48
-#Kids        0 days 00:02:01
-#Vit         4 days 07:36:39
+#changing Duration to time account
+df1['Duration'] = pd.to_timedelta(df1['Duration'])
+
+# examining individual users hours spent on Netflix
+df1['Duration'].groupby(df1['Profile Name']).sum()
+
+#Profile Name
+#Karina     90 days 01:52:53
+#Karolina   21 days 17:37:15
+#Vit         4 days 06:26:10
 
 
 
 # Examines the Profile Names 
 plt.figure(figsize = (12, 8))
-sns.countplot(x = 'Profile Name', data = dataset, palette = 'viridis', order = dataset['Profile Name'].value_counts().index)
+sns.countplot(x = 'Profile Name', data = df1, palette = 'viridis', order = df1['Profile Name'].value_counts().index)
 plt.xticks(rotation = 90)
 plt.title('Breakdown of Profile Name', fontsize = 16)
 plt.ylabel('count', fontsize = 14)
@@ -123,29 +132,30 @@ Also: this just the number of times the user was logged on.
 # =============================================================================
 # Creating Weekdays 
 # =============================================================================
+# creating a weekday variable
+df1['weekdays'] =  df1['Start Time'].dt.weekday
 
-dataset['weekdays'] =  dataset['Start Time'].dt.weekday
 
-
-
-dataset['weekdays'] = pd.Categorical(dataset['weekdays'], 
+# making 'weekdays' a categorical variable
+df1['weekdays'] = pd.Categorical(df1['weekdays'], 
        categories = [0,1,2,3,4,5,6],
        ordered = True)
 
 
+
 # find the table
-dataset.groupby('Profile Name')['weekdays'].value_counts()
+df1.groupby('Profile Name')['weekdays'].value_counts()
 
 
 # Manual insertion of data. Is there a better way to do this?
-new_dataset = pd.DataFrame({
+new_df1 = pd.DataFrame({
     "Karina":[820,880,911,994,1187,1137,848],
     "Karolina":[304,243,289,295,234,562,960],
     "Vit":[80, 92, 92, 72, 125,121, 113]
     }, 
     index=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", 'Sun']
 )
-dataset.plot(kind="bar")
+new_df1.plot(kind="bar")
 plt.title("Netflix Consumption Study")
 plt.xlabel("Family Member")
 plt.ylabel("Neftlix")
@@ -154,13 +164,13 @@ plt.ylabel("Neftlix")
 
 
 # find the table
-views_per_day = dataset.groupby(
+views_per_day = df1.groupby(
         ['Profile Name', 'weekdays']
-        )['Duration'].count().reset_index()
+        )['Duration'].sum().reset_index()
 
 
-plotdata = views_per_day.pivot(index = 'Profile Name',
-                                columns = 'weekdays',
+plotdata = views_per_day.pivot(index = 'weekdays',
+                                columns = 'Profile Name',
                                 values = 'Duration')
 
 plotdata.plot(kind = 'bar')
@@ -174,14 +184,14 @@ plotdata.plot(kind = 'bar')
 # Creating Hour
 # =============================================================================
 
-dataset['hour'] = dataset['Start Time'].dt.hour
+df1['hour'] = df1['Start Time'].dt.hour
 
-dataset['hour'] = pd.Categorical(dataset['hour'], 
+df1['hour'] = pd.Categorical(df1['hour'], 
        categories = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
        ordered = True)
 
 # find the table
-views_per_user = dataset.groupby(
+views_per_user = df1.groupby(
         ['Profile Name', 'hour']
         )['Duration'].count().reset_index()
 
@@ -192,36 +202,71 @@ plotdata1 = views_per_user.pivot(index = 'Profile Name',
 
 plotdata1.plot(kind = 'bar')
 
-dataset.groupby('Profile Name')['hour'].value_counts()
+df1.groupby('Profile Name')['hour'].value_counts()
+
+# =============================================================================
+# Creating Type: Tv Show or Movie
+# =============================================================================
+
+#trying to find the all the names with Season and create a new variable called Type
+df1.loc[df1['Title'].str.contains('Season'), 'Type'] = 'TV Show'
+
+#Trying to create Movie in Type
+df1.loc[df1['Type'].isnull(), 'Type'] = 'Movie'
+
+
+#visualising duration and profile Name type
+views_per_user_by_Type = df1.groupby(
+        ['Profile Name', 'Type']
+        )['Duration'].sum().reset_index()
+
+#Creating table to view Duration by Name and Type
+pivot_movie_name = pd.DataFrame({'A': df1['Profile Name'],
+                   'B': df1['Type'],
+                   'C': df1['Duration']})
+
+# Pivoting the talbe
+pivot_movie_name = pd.pivot_table(pivot_movie_name, values='C', index=['A'], columns=['B'], aggfunc='sum')
+
+#Produce the table
+print(pivot_movie_name)
+#B                   Movie          TV Show
+#A                                         
+#Karina   22 days 03:48:58 67 days 22:03:55
+#Karolina 10 days 04:41:23 11 days 12:55:52
+#Vit       1 days 10:28:40  2 days 19:57:30
+
+plotdata2 = views_per_user_by_Type.pivot(index = 'Profile Name',
+                                columns = 'Type',
+                                values = 'Duration')
+
+plotdata2.plot(kind = 'bar')
+
+# =============================================================================
+# Splitting Titles
+# =============================================================================
+
+
+for title in df.Title:
+    title_name = []
+    if df[df['Title'].str.contains('Season', regex=False)]:
+        df['Title'].lsplit('; ')
+        title_name.append(str(df['Title']).lstrip(' :'))
+return title_name
 
 
 
 
-Karina = dataset[dataset['Profile Name'] == 'Karina']
+if df[df['Title'].any().contains('Season', regex=False)]:
+df['Title'].lsplit('; ')
 
-views_for_Karina = Karina.groupby(['hour'])['Duration'].count().reset_index()
 
-
-views_for_Karina.plot(kind = 'bar')
-
+seasons = df1[df1['Title'].str.contains('Season', regex=False)]
 
 
 
+def remove_title(text):
+        clean_Title = [word for word in text.split() if text.str.contains('Sason')]
+        return clean_Title
 
-Karolina = dataset[dataset['Profile Name'] == 'Karolina']
-
-views_for_Karolina = Karolina.groupby(['hour'])['Duration'].count().reset_index()
-
-
-views_for_Karolina.plot(kind = 'bar')
-
-
-
-
-
-Vit = dataset[dataset['Profile Name'] == 'Vit']
-
-views_for_Vit = Vit .groupby(['hour'])['Duration'].count().reset_index()
-
-
-views_for_Vit .plot(kind = 'bar')
+df1['Title_Name']=df1['Title'].apply(remove_title)
